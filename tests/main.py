f6883b95c2dbe705ca464c7f5f197e4efe45a814
Simon Vandevelde <s.vandevelde@kuleuven.be>
2020-05-20 11:20:19 +0200

    PEP8

--- tests/main.py
+++ tests/main.py
@@ -1,41 +1,40 @@
 
-import inspect, os,sys,time, pprint
+import inspect, os, sys, time, pprint
 
 # add parent folder to import path
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir)
-sys.path.insert(0,os.path.join(parentdir, 'Idp'))
+sys.path.insert(0, parentdir)
+sys.path.insert(0, os.path.join(parentdir, 'Idp'))
 
 from Case import Case
-from Inferences import *
-from Solver import *
-from debugWithYamlLog import Log, Log_file, nl, log_calls
-from utils import log, start
+from Inferences import metaJSON, propagation
+from debugWithYamlLog import Log_file, log_calls
+from utils import start
 from Idp import idpparser, SymbolDeclaration
 from Idp.Expression import Expression, AppliedSymbol, Variable, Fresh_Variable
+from typing import Dict
 
-from typing import Dict, Any
 
-# patch Log on Idp.Substitute ##############################################################
+# patch Log on Idp.Substitute #################################################
 
 for i in [Expression, AppliedSymbol, Variable, Fresh_Variable]:
     i.substitute = log_calls(i.substitute)
 
 
-###########################################################################################
+###############################################################################
 
 dir = os.path.dirname(__file__)
-files  = [os.path.join(dir, "1 sandbox/sandbox.idp")]
-files += [x[0]+"/"+f for x in os.walk(dir) for f in x[2] 
-    if f.endswith(".idp") and not f.endswith("sandbox.idp")]
+files = [os.path.join(dir, "1 sandbox/sandbox.idp")]
+files += [x[0]+"/"+f for x in os.walk(dir) for f in x[2] if
+          f.endswith(".idp") and not f.endswith("sandbox.idp")]
 files.sort()
 for file in files:
     print(file)
     Log_file(file)
     f = open(file, "r")
     theory = f.read()
-    idp = idpparser.model_from_str(theory)  
+    idp = idpparser.model_from_str(theory)
 
     expanded_symbols: Dict[str, SymbolDeclaration] = {}
     for expr in idp.subtences.values():
@@ -53,28 +52,30 @@ for file in files:
     f.write("\r\n-- vocabulary -------------------------------\r\n")
     f.write("\r\n\r\n".join(str(t) for t in case.idp.vocabulary.translated))
     f.write("\r\n-- theory -----------------------------------\r\n")
-    f.write("\r\n\r\n".join(str(t) for t in case.idp.theory.translate(case.idp))     + "\r\n")
+    f.write("\r\n\r\n"
+            .join(str(t) for t in case.idp.theory.translate(case.idp))
+            + "\r\n")
     if case.idp.goal.translate() is not None:
         f.write("\r\n-- goal -----------------------------------\r\n")
-        f.write(str(case.idp.goal.translate())     + "\r\n")
+        f.write(str(case.idp.goal.translate()) + "\r\n")
     f.write("\r\n-- subtences ------------------------------------\r\n")
-    f.write("\r\n".join(str(t) for t in case.idp.subtences)     + "\r\n")
+    f.write("\r\n".join(str(t) for t in case.idp.subtences) + "\r\n")
     f.write("\r\n-- GUILines ------------------------------------\r\n")
-    f.write("\r\n".join(str(t) for t in case.GUILines)     + "\r\n")
+    f.write("\r\n".join(str(t) for t in case.GUILines) + "\r\n")
 
     f.write("\r\n-- case -------------------------------------\r\n")
-    f.write(str(case)+ "\r\n")
+    f.write(str(case) + "\r\n")
     f.write("\r\n-- meta -------------------------------------\r\n")
-    out = pprint.pformat(metaJSON(case.idp), width = 120)
-    f.write(out     + "\r\n")
+    out = pprint.pformat(metaJSON(case.idp), width=120)
+    f.write(out + "\r\n")
     f.write("\r\n-- propagation ------------------------------\r\n")
-    out = pprint.pformat(propagation(case), width = 120)
-    f.write(out     + "\r\n")
+    out = pprint.pformat(propagation(case), width=120)
+    f.write(out + "\r\n")
 
     f.close()
     print("----------------")
 
-total = round(time.process_time()-start,3)
+total = round(time.process_time()-start, 3)
 print("*** Total: ", total)
 f = open(os.path.join(dir, "duration.txt"), "w")
 f.write(str(total))
