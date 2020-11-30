e013614b565b323a4628c4a612c1b0f27b496aa6
Pierre <pierre.carbonnelle@gmail.com>
2020-11-30 21:09:01 +0100

    docs: zettlr

--- Idp/Parse.py
+++ Idp/Parse.py
@@ -689,6 +689,7 @@ class SymbolInterpretation(object):
                     struct.assignments.assert_(expr, self.default, Status.STRUCTURE, False)
 
     def interpret(self, theory, rank, applied, args, tuples=None):
+        """ returns the interpretation of self applied to args """
         tuples = self.enumeration.tuples if tuples == None else tuples
         if rank == self.decl.arity:  # valid tuple -> return a value
             if not self.decl.function:
