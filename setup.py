47bc0c80945d04260b0869a5beec7b611bd74a86
Simon Vandevelde <s.vandevelde@kuleuven.be>
2020-12-18 13:16:40 +0100

    Remove Cython

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules = cythonize(["idp_solver/Substitute.py"], annotate=True, language_level=3),
-    zip_safe=False,
-)
