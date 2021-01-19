391ffcc2d1a34904a5357f0e1d3083d8c1dfd247
Simon Vandevelde <s.vandevelde@kuleuven.be>
2021-01-19 08:02:46 +0100

    Add outputfile and timing

--- idp_solver/IDP_Z3.py
+++ idp_solver/IDP_Z3.py
@@ -25,25 +25,47 @@ The command-line program to execute an IDP file with a main() block.
 import argparse
 import os
 import sys
+import time
 
 from idp_solver import idpparser
+
+
+def run_idp(idp):
+    start = time.time()
+    idp = idpparser.model_from_str(idp)
+    idp.execute()
+    return time.time() - start
 
 
 def cli(args=None):
     parser = argparse.ArgumentParser(description='IDP-Z3')
     parser.add_argument('inputfile', type=str,
                         help='path to the input file')
+    parser.add_argument('--outputfile', '-o', type=str,
+                        help='path to the output file')
     args = parser.parse_args()
-
     error = 0
 
     inputf = os.path.abspath(args.inputfile)
     with open(inputf, "r") as f:
-        theory = f.read()
+        idp = f.read()
 
-    idp = idpparser.model_from_str(theory)
-    idp.execute()
+    time = 0
+    if not args.outputfile:
+        time = run_idp(idp)
+    else:
+        with open(args.outputfile, "w") as out:
+            from io import StringIO
+            old = sys.stdout
+            sys.stdout = stdout = StringIO()
+            sys.stdout = stdout
 
+            time = run_idp(idp)
+
+            sys.stdout = old
+            out.write(str(stdout.getvalue()))
+
+    print("Elapsed time: {} seconds".format(time))
     sys.exit(error)
 
 
