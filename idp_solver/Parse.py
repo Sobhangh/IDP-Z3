c8e3de8468cc43f50d73a0fe8c0f89da03a9dbce
Simon Vandevelde <s.vandevelde@kuleuven.be>
2021-01-22 15:07:39 +0100

    More dead code

--- idp_solver/Parse.py
+++ idp_solver/Parse.py
@@ -1041,7 +1041,6 @@ class Display(object):
                 # All arguments should be symbols, except for the first
                 # argument of 'unit' and 'category'.
                 for i, symbol in enumerate(constraint.sub_exprs):
-                    assert isinstance(symbol, Constructor), f"argument '{str(symbol)}' of '{constraint.name}' should be a Constructor, not a {type(symbol)}"
                     if constraint.name in ['unit', 'category'] and i == 0:
                         continue
 
