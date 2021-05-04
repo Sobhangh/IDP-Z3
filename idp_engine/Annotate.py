00df641f465d999b141160b9b952a1361660c00a
Pierre <pierre.carbonnelle@gmail.com>
2021-05-04 09:15:04 +0200

    docs: improve error message

--- idp_engine/Annotate.py
+++ idp_engine/Annotate.py
@@ -74,7 +74,7 @@ def annotate(self, voc):
     for c in self.constructors:
         c.type = self.name
         self.check(c.name not in voc.symbol_decls or self.name == SYMBOL,
-                    f"duplicate constructor in vocabulary: {self.name}")
+                    f"duplicate '{c.name}' constructor for '{self.name}' type")
         voc.symbol_decls[c.name] = c
     if self.interpretation:
         self.interpretation.annotate(voc)
@@ -222,7 +222,7 @@ def annotate(self, block):
         for c in self.enumeration.constructors:
             c.type = self.name
             self.check(c.name not in voc.symbol_decls,
-                    f"duplicate constructor in vocabulary: {self.name}")
+                    f"duplicate '{c.name}' constructor for '{self.name}' symbol")
             voc.symbol_decls[c.name] = c  #TODO risk of side-effects => use local decls ? issue #81
 
     self.enumeration.annotate(voc)
