3b14d6936c42571ed7d3c75d633e6916fb71718d
Sobhan Gharanfoli <sobhan.gharanfoli@student.kuleuven.be>
2023-11-13 15:12:16 +0100

    temp

--- idp_engine/Parse.py
+++ idp_engine/Parse.py
@@ -509,12 +509,12 @@ class SymbolDeclaration(ASTNode):
     """
 
     def __init__(self, **kwargs):
-        #temp_symbol = kwargs.pop('temp')
+        temp_symbol = kwargs.pop('temp')
         self.annotations : Annotations = kwargs.pop('annotations')
         self.symbols : Optional[List[Symbol]]
         self.name : Optional[str]
-        #self.temp = True if temp_symbol else False
-        self.temp= False
+        self.temp = True if temp_symbol else False
+        #self.temp= False
         if 'symbols' in kwargs:
             self.symbols = kwargs.pop('symbols')
             self.name = None
