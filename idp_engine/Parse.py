d400f144389a731c209c5c320e9e8fed3058f519
Sobhan Gharanfoli <sobhan.gharanfoli@student.kuleuven.be>
2023-11-24 20:50:28 +0100

    t

--- idp_engine/Parse.py
+++ idp_engine/Parse.py
@@ -411,7 +411,7 @@ class Vocabulary(ASTNode):
                 if isinstance(d,SymbolDeclaration):
                     print("temporal symbol")
                     print(t.symbol)
-                    if d.name == t.symbol:
+                    if str(d.name) == str(t.symbol):
                         print(d.name)
                         changed = True
                         id = SymbolDeclaration(name=d.name,sorts=d.sorts,out=d.out,annotations=Annotations(None,[]))
